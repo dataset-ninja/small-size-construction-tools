9d538cd38e8bf021c12f4f1fbd6145dddcf90901
Ars Vladimirov <grokhael@gmail.com>
2023-07-05 01:55:18 +0400

    cahnge order

--- src/convert.py
+++ src/convert.py
@@ -189,10 +189,10 @@ def convert_and_upload_supervisely_project(
     progress = sly.Progress("Create dataset '{}'".format(ds_name), jpg_count)
 
     for folderpath in [
-        "DATA1/DATA1",
-        "DATA2/DATA2",
         "DATA3/DATA3",
         "DATA4/DATA4",
+        "DATA1/DATA1",
+        "DATA2/DATA2",
     ]:
         curpath = os.path.join(dataset_path, folderpath)
 
