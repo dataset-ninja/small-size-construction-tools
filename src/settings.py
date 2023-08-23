8f1f964931a2ce2197f20e2fe5d5a17787b2a817
Arsenii Vladimirov <78355358+grokhi@users.noreply.github.com>
2023-08-23 13:01:09 +0600

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -15,6 +15,7 @@ from dataset_tools.templates import (
 ##################################
 PROJECT_NAME: str = "Detection of Small Size Construction Tools"
 PROJECT_NAME_FULL: str = "Image Dataset for Object Detection of Small Size Construction Tools"
+HIDE_DATASET = False  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
@@ -108,6 +109,7 @@ def get_settings():
     settings = {
         "project_name": PROJECT_NAME,
         "license": LICENSE,
+        "hide_dataset": HIDE_DATASET,        
         "applications": APPLICATIONS,
         "category": CATEGORY,
         "cv_tasks": CV_TASKS,
